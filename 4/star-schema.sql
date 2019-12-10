29e77487e5b62f80ee7ce994f3bcce630e934bf1
tiago-msg <tiago.manuel.s.goncalves@gmail.com>
2019-12-10 17:15:36 +0000

    Início do star-schema

---
+++ 4/star-schema.sql
@@ -0,0 +1,50 @@
+DROP TABLE IF EXISTS d_utilizador CASCADE;
+DROP TABLE IF EXISTS d_tempo CASCADE;
+DROP TABLE IF EXISTS d_local CASCADE;
+DROP TABLE IF EXISTS d_lingua CASCADE;
+DROP TABLE IF EXISTS f_anomalia CASCADE;
+
+----------------------------------------
+-- Tables
+----------------------------------------
+
+CREATE TABLE d_utilizador
+   (id_utilizador           INTEGER	        NOT NULL,
+    email 	                VARCHAR(255)	NOT NULL,
+    tipo 	                VARCHAR(80)	    NOT NULL,
+    PRIMARY KEY(id_utilizador));
+
+CREATE TABLE d_tempo
+   (id_tempo                INTEGER	        NOT NULL,
+    dia 	                DATE	        NOT NULL,
+    dia_da_semana           VARCHAR(10)	    NOT NULL,
+    semana                  INTEGER	        NOT NULL,
+    mes                     VARCHAR(10)	    NOT NULL,
+    trimestre               INTEGER	        NOT NULL,
+    ano                     INTEGER	        NOT NULL,
+    PRIMARY KEY(id_tempo));
+
+CREATE TABLE d_local
+   (id_local                INTEGER	        NOT NULL,
+    latitude 	            FLOAT	        NOT NULL,
+    longitude 	            FLOAT	        NOT NULL,
+    nome 	                VARCHAR(255)	NOT NULL,
+    PRIMARY KEY(id_local));
+
+CREATE TABLE d_lingua
+    (id_lingua              INTEGER         NOT NULL,
+     lingua 	            VARCHAR(255)	NOT NULL,
+     PRIMARY KEY(id_lingua));
+
+
+CREATE TABLE f_anomalia
+    (id_utilizador           INTEGER	    NOT NULL,
+     id_tempo                INTEGER	    NOT NULL,
+     id_local                INTEGER        NOT NULL,
+     id_lingua               INTEGER        NOT NULL,
+     tipo_anomalia           VARCHAR(80)	NOT NULL,
+     com_porposta            BOOLEAN        NOT NULL,
+     FOREIGN KEY(id_utilizador) REFERENCES d_utilizador(id_utilizador) ON DELETE CASCADE ON UPDATE CASCADE,
+     FOREIGN KEY(id_tempo) REFERENCES d_tempo(id_tempo) ON DELETE CASCADE ON UPDATE CASCADE,
+     FOREIGN KEY(id_local) REFERENCES d_local(id_local) ON DELETE CASCADE ON UPDATE CASCADE,
+     FOREIGN KEY(id_lingua) REFERENCES d_lingua(id_lingua) ON DELETE CASCADE ON UPDATE CASCADE);
