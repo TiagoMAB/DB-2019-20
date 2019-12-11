dfe5efcf230128c492f54ef6efbad20d669431e8
Daniel Pereira <danielarp014@hotmail.com>
2019-12-11 12:51:09 +0000

    correction to previous commit

--- 4/star-schema_populate.sql
+++ 4/star-schema_populate.sql
@@ -15,5 +15,5 @@ INSERT INTO d_lingua(lingua) (SELECT lingua FROM anomalia);
 --INSERT INTO f_anomalia(id_utilizador, id_tempo, id_local, id_lingua, tipo_anomalia, com_porposta) 
 --    (SELECT id_utilizador, id_tempo, id_local, id_lingua, 'anomalia_redacao', incidencia.anomalia_id IN (SELECT anomalia_id FROM correcao)
 --    FROM incidencia INNER JOIN anomalia ON anomalia.id = incidencia.anomalia_id INNER JOIN item ON item.id = incidencia.item_id INNER JOIN local_publico ON item.latitude = local_publico.latitude AND item.longitude = local_publico.longitude 
---         INNER JOIN d_utilizador ON incidencia.email = d_utilizador.email INNER JOIN d_tempo ON (something) INNER JOIN d_local ON d_local.latitude = item.latitude AND d_local.longitude = item.longitude INNER JOIN d_lingua ON 
+--         INNER JOIN d_utilizador ON incidencia.email = d_utilizador.email INNER JOIN d_tempo ON (something) INNER JOIN d_local ON d_local.latitude = item.latitude AND d_local.longitude = item.longitude INNER JOIN d_lingua ON anomalia.lingua = d_lingua.lingua
 --    WHERE tem_anomalia_redacao = TRUE);
