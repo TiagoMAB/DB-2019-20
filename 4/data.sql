d42d2d6ef023993b4ee9d337bc4f33bebbf60028
TiagoMAB <tiago.barroso1@hotmail.com>
2019-12-13 16:02:27 +0000

    fixes

--- 4/data.sql
+++
@@ -1,4 +0,0 @@
-SELECT tipo_anomalia, lingua, dia_da_semana, COUNT(*)
-FROM f_anomalia NATURAL JOIN d_lingua NATURAL JOIN d_tempo
-GROUP BY CUBE (tipo_anomalia, lingua, dia_da_semana)
-ORDER BY (tipo_anomalia, lingua, dia_da_semana)
